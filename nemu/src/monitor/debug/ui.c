7568aab89a65d5456dcaa532382ff0db25a590b6
tracer-ics2015 <tracer@njuics.org>
2015-09-08 16:48:15 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:48:15 up  3:37,  5 users,  load average: 0.00, 0.02, 0.05 38766e392d65c22ccf764c5725c19bf14be7af1a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,8 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_si(char *args);
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +48,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+	{ "si", "Dan Bu Tiao Shi", cmd_si }
 	/* TODO: Add more commands */
 
 };
@@ -74,6 +76,11 @@ static int cmd_help(char *args) {
 		printf("Unknown command '%s'\n", arg);
 	}
 	return 0;
+}
+
+static int cmd_si(char *args){
+	//cpu_exec(-1);
+	return 0;	
 }
 
 void ui_mainloop() {
