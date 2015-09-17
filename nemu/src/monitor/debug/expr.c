a9702406b2f01f1806c6f84ad4a7f633d2f9d837
tracer-ics2015 <tracer@njuics.org>
2015-09-17 18:14:16 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:14:16 up  1:52,  4 users,  load average: 0.00, 0.01, 0.05 87566a4914645c36ab34b4c76048e9a174186209

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -100,7 +100,7 @@ uint32_t expr(char *e, bool *success) {
 		*success = false;
 		return 0;
 	}
-	printf("^ ^");
+	printf("\n^ ^");
 	/* TODO: Insert codes to evaluate the expression. */
 	panic("please implement me");
 	return 0;
