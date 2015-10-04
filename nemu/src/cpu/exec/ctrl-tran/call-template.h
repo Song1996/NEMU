38594ace44b33fb14b1f5bdfa3f923a3e1786f80
tracer-ics2015 <tracer@njuics.org>
2015-10-04 14:02:57 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:02:57 up  2:35,  5 users,  load average: 0.00, 0.01, 0.05 6e2fb09895f6e0fbf2f0ab9cb27faa13d7583b0f

--- nemu/src/cpu/exec/ctrl-tran/call-template.h
+++ nemu/src/cpu/exec/ctrl-tran/call-template.h
@@ -5,7 +5,8 @@
 static void do_execute(){
 	int src = op_src->val;
 	cpu.eip += src;
-	//print_asm_template();
+//	print_asm_template();
+	printf("call $0x%x",src);
 }
 //#if DATA_BYTE==2||DATA_BYPE==4
 make_instr_helper(i)
