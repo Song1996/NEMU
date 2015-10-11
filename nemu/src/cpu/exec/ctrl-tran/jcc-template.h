7f64b8d3dcd858d1939bc6a447476a16f45d335c
tracer-ics2015 <tracer@njuics.org>
2015-10-11 12:59:39 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:59:39 up  1:57,  5 users,  load average: 0.00, 0.01, 0.05 c4377633e524f8ed79331a964e3ea89fd94e7b1

--- nemu/src/cpu/exec/ctrl-tran/jcc-template.h
+++ nemu/src/cpu/exec/ctrl-tran/jcc-template.h
@@ -82,6 +82,76 @@ make_helper(concat(jecxz_i_,SUFFIX))
 	return len+1;
 }
 
+make_helper(concat(jz_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.ZF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+
+make_helper(concat(jg_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if((!cpu.ZF)&&(cpu.SF==cpu.OF))
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+
+make_helper(concat(jge_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.SF==cpu.OF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+
+make_helper(concat(jl_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.SF!=cpu.OF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+
+make_helper(concat(jle_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.ZF||cpu.SF!=cpu.OF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+
+make_helper(concat(jna_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.CF||cpu.ZF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+
+make_helper(concat(jnae_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.CF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+
 
 
 
--- nemu/src/cpu/exec/ctrl-tran/jcc.h
+++ nemu/src/cpu/exec/ctrl-tran/jcc.h
@@ -9,6 +9,12 @@ make_helper(jc_i_b);
 make_helper(jcxz_i_b);
 make_helper(jecxz_i_b);
 make_helper(je_i_b);
+make_helper(jz_i_b);
+make_helper(jg_i_b);
+make_helper(jge_i_b);
+make_helper(jl_i_b);
+make_helper(jle_i_b);
+
 
 make_helper(ja_i_v);
 make_helper(jae_i_v);
@@ -16,5 +22,10 @@ make_helper(jb_i_v);
 make_helper(jbe_i_v);
 make_helper(jc_i_v);
 make_helper(je_i_v);
+make_helper(jz_i_v);
+make_helper(jg_i_v);
+make_helper(jge_i_v);
+make_helper(jl_i_v);
+make_helper(jle_i_v);
 
 #endif
