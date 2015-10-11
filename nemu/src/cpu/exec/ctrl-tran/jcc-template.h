f7d7ce259029c6c339fe5ba182fbabde39780d09
tracer-ics2015 <tracer@njuics.org>
2015-10-11 12:08:29 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:08:29 up  1:06,  5 users,  load average: 0.00, 0.01, 0.05 2d245bf6bb3ed74172eec0b5a6caede039e0efeb

--- nemu/src/cpu/exec/ctrl-tran/jcc-template.h
+++ nemu/src/cpu/exec/ctrl-tran/jcc-template.h
@@ -1,14 +1,20 @@
 #include "cpu/exec/template-start.h"
-
-#define instr je
-
+/*
 static void do_execute(){
 	int src=op_src->val;
 	if(cpu.ZF)
 		cpu.eip += src;
 	print_asm_template1();
-}
+}*/
 
-make_instr_helper(i)
+make_helper(concat(je_i_, SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.ZF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
 #include "cpu/exec/template-end.h"
