703b08ddf9d50430627af0c3fba2ef3d64b3a55a
tracer-ics2015 <tracer@njuics.org>
2015-10-11 13:28:19 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:28:19 up  2:26,  5 users,  load average: 0.00, 0.01, 0.05 f9b29dac151267e412acb378cd7390c3555fbe5c

--- nemu/src/cpu/exec/ctrl-tran/jcc-template.h
+++ nemu/src/cpu/exec/ctrl-tran/jcc-template.h
@@ -152,27 +152,185 @@ make_helper(concat(jnae_i_,SUFFIX))
 	return len+1;
 }
 
+make_helper(concat(jnb_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(!cpu.CF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jnbe_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if((!cpu.CF)&&(!cpu.ZF))
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jnc_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(!cpu.CF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jne_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(!cpu.ZF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jng_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if((cpu.SF!=cpu.OF)||(cpu.ZF))
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jnge_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.SF!=cpu.OF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jnl_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.SF==cpu.OF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jnle_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if((cpu.SF==cpu.OF)||(!cpu.ZF))
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jno_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(!cpu.OF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jnp_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(!cpu.PF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jns_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(!cpu.SF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jnz_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(!cpu.ZF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jo_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.OF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jp_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.PF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jpe_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.PF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
+make_helper(concat(jpo_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(!cpu.PF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
 
-
-
-
-
-
-
+make_helper(concat(js_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.SF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+/*
+make_helper(concat(jz_i_,SUFFIX))
+{
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	if(cpu.ZF)
+		cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+*/
 
 #include "cpu/exec/template-end.h"
