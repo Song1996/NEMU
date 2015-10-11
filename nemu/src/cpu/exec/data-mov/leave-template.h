0e52ffdb1428e470314cd0728690da1fbe20c874
tracer-ics2015 <tracer@njuics.org>
2015-10-11 18:45:04 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:45:04 up  7:43,  5 users,  load average: 0.00, 0.01, 0.05 d580082c3187641f21b77c774f2480b0683446fa

---
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -0,0 +1,15 @@
+#include "cpu/exec/template-start.h"
+
+#define instr leave
+
+make_helper(concat(leave_,SUFFIX))
+{
+	cpu.esp=cpu.ebp;
+	cpu.ebp=MEM_R(cpu.esp);
+	cpu.esp+=DATA_BYTE;
+	print_asm("leave");
+	return 1;
+}
+
+
+#include "cpu/exec/template-end.h"
