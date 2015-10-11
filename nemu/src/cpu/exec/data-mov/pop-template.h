b0970cd18a5a912511ce7b47ea0a023c478bb216
tracer-ics2015 <tracer@njuics.org>
2015-10-11 18:21:09 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:21:09 up  7:19,  5 users,  load average: 0.00, 0.01, 0.05 4aff7a48d79e52349498596b8d984f413ba10153

---
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -0,0 +1,15 @@
+#include "cpu/exec/template-start.h"
+
+#define instr pop
+
+static void do_execute() {
+	swaddr_t  addr=op_src->val;
+	MEM_W(addr,MEM_R(cpu.esp));
+	cpu.esp+=DATA_BYTE;
+	print_asm_template2();
+}
+
+make_instr_helper(rm)
+make_instr_helper(r)
+
+#include "cpu/exec/template-end.h"
