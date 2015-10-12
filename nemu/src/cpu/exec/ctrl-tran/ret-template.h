efba37bd7559cce30863c96e3eb2f4c3ffc45f6e
tracer-ics2015 <tracer@njuics.org>
2015-10-12 23:27:59 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:27:59 up  2:18,  7 users,  load average: 0.01, 0.03, 0.05 9494f10c482f25c721028899c0feed698b436f08

--- nemu/src/cpu/exec/ctrl-tran/ret-template.h
+++ nemu/src/cpu/exec/ctrl-tran/ret-template.h
@@ -1,11 +1,13 @@
 #include "cpu/exec/template-start.h"
 
+#define instr ret
 
 make_helper(concat(ret_,SUFFIX))
 {
 	cpu.eip=MEM_R(cpu.esp);
 	cpu.esp+=DATA_BYTE;
-	print_asm_template1();
+	print_asm("ret\n");
+	cpu.eip--;
 	return 1;
 }
 
