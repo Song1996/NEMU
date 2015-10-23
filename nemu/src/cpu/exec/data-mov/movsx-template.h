359047e67ace60333047d7bddb61ee48b33f981e
tracer-ics2015 <tracer@njuics.org>
2015-10-23 20:28:22 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:28:22 up  1:26,  3 users,  load average: 0.00, 0.01, 0.05 29af8cbb32c177dccfb4ee4e616c0952942749f0

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -1,11 +1,11 @@
 #include "cpu/exec/template-start.h"
 
-#define instr movzx
+#define instr movsx
 
-make_helper(concat(movzx_rm2r_,SUFFIX))
+make_helper(concat(movsx_rm2r_,SUFFIX))
 {
 	int len=concat(decode_rm2r_,SUFFIX)(eip+1);
-	unsigned int src=op_src->val;
+	int src=op_src->val;
 	if (DATA_BYTE==1)
 	{
 		src<<=24;
@@ -14,15 +14,15 @@ make_helper(concat(movzx_rm2r_,SUFFIX))
 	else if(DATA_BYTE==2)
 	{
 		src<<=16;
-		src>>=16;	
+		src>>=16;
 	}
 	//else
-	//	nemu_assert();
-	OPERAND_W(op_dest,src);
+	//  nemu_assert();
+    OPERAND_W(op_dest,src);
 	print_asm_template2();
 	return len+1;
 }
 
-//make_helper(rm2r)
+//make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
