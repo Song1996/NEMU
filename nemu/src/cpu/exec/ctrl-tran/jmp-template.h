fbf89d732f6e76a25fe2f5b34b6797b13015ea60
tracer-ics2015 <tracer@njuics.org>
2015-10-23 20:04:23 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:04:23 up  1:02,  2 users,  load average: 0.08, 0.13, 0.15 b3f3564fcd196789d4096d5c57789476460cfa4a

--- nemu/src/cpu/exec/ctrl-tran/jmp-template.h
+++ nemu/src/cpu/exec/ctrl-tran/jmp-template.h
@@ -2,13 +2,23 @@
 
 #define instr jmp
 
-static void do_execute(){
+make_helper(concat(jmp_si_,SUFFIX)){
+	int len=concat(decode_si_,SUFFIX)(eip+1);
 	int src=op_src->val;
 	cpu.eip+=src;
 	print_asm_template1();
+	return len+1;
 }
 
-make_instr_helper(si)
-make_instr_helper(rm)
+make_helper(concat(jmp_rm_,SUFFIX)){
+	int len=concat(decode_rm_,SUFFIX)(eip+1);
+	int src=op_src->val;
+	cpu.eip+=src;
+	print_asm_template1();
+	return len+1;
+}
+
+//make_instr_helper(si)
+//make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
