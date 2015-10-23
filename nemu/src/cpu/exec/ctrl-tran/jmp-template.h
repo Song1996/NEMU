fb4ea4c9d11ddee0bc3cf542a29324baccbb5fd2
tracer-ics2015 <tracer@njuics.org>
2015-10-23 20:11:48 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:11:48 up  1:09,  3 users,  load average: 0.00, 0.04, 0.11 3ae2ff7c8335d1960142cb8f2cd1f35b19e4b08

--- nemu/src/cpu/exec/ctrl-tran/jmp-template.h
+++ nemu/src/cpu/exec/ctrl-tran/jmp-template.h
@@ -11,11 +11,11 @@ make_helper(concat(jmp_si_,SUFFIX)){
 }
 
 make_helper(concat(jmp_rm_,SUFFIX)){
-	int len=concat(decode_rm_,SUFFIX)(eip+1);
+	//int len=concat(decode_rm_,SUFFIX)(eip+1);
 	int src=op_src->val;
 	cpu.eip=src;
 	print_asm_template1();
-	return len+1;
+	return 0;
 }
 
 //make_instr_helper(si)
