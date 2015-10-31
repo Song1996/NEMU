65af2e63072b8ffe48ff99f9360dbc8c459ab8a5
tracer-ics2015 <tracer@njuics.org>
2015-10-31 23:49:01 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:49:01 up  2:44,  3 users,  load average: 0.15, 0.09, 0.09 dcd495ae2b167b81f3b6eb36367330b5ee2c14a

---
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -0,0 +1,35 @@
+#include "cpu/exec/template-start.h"
+
+make_helper(concat(cmps_,SUFFIX))
+{
+		DATA_TYPE left=MEM_R(REG(R_ESI));
+			DATA_TYPE right=MEM_R(REG(R_EDI));
+				DATA_TYPE result=left-right;
+					if(MSB(left)!=MSB(right) && MSB(left)!=MSB(result))
+								cpu.OF=1;
+						else
+									cpu.OF=0;
+							cpu.SF=!!MSB(result);
+								cpu.ZF=!result;
+									if(result>left)
+												cpu.CF=1;
+										else
+													cpu.CF=0;
+											int i,count=0;
+												for(i=0;i<8;i++)
+															if((result>>i)&0x1)	count++;
+													if(count % 2) cpu.PF=0;
+														else cpu.PF=1;
+															if((result&0xF)>(left&0xF))
+																		cpu.AF=1;
+																else
+																			cpu.AF=0;
+																	if(DATA_BYTE==1) print_asm("cmpsb");
+																		else if(DATA_BYTE==2) print_asm("cmpsw");
+																			else print_asm("cmpsl");
+																				return 1;
+}
+
+
+#include "cpu/exec/template-end.h"
+
