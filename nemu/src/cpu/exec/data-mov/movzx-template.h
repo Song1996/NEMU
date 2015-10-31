9606154f60a6e9e0a05e819b5a4536d8ab687e94
tracer-ics2015 <tracer@njuics.org>
2015-10-31 23:57:18 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:57:19 up  2:52,  3 users,  load average: 0.00, 0.02, 0.05 66f892e18185a0fb25210bb02af822d53b25ba54

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -1,12 +1,13 @@
 #include "cpu/exec/template-start.h"
 
 #define instr movzx
-
+#if DATA_BYTE==2||DATA_BYTE==4
 make_helper(concat(movzx_rm2r_,SUFFIX))
 {
 	int len=concat(decode_rm2r_,SUFFIX)(eip+1);
 	uint8_t result=op_src->val;
-	OPERAND_W(op_dest,result);
+	DATA_TYPE temp=result;
+	OPERAND_W(op_dest,temp);
 	print_asm_template2();
 	return len+1;
 }
@@ -14,10 +15,12 @@ make_helper(concat(movzx2_rm2r_,SUFFIX))
 {
 	int len=concat(decode_rm2r_,SUFFIX)(eip+1);
 	uint16_t result=op_src->val;
-	OPERAND_W(op_dest,result);
+	DATA_TYPE temp=result;
+	OPERAND_W(op_dest,temp);
 	print_asm_template2();
 	return len+1;
 }
+#endif
 //make_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
