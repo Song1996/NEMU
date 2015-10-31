7905b5c922f04b3c4031fa15ba7c0e4625b9473a
tracer-ics2015 <tracer@njuics.org>
2015-10-31 23:21:14 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:21:14 up  2:16,  3 users,  load average: 0.04, 0.04, 0.05 2620a299cacc23dce8d477a98e934cfc5f8aa350

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -1,7 +1,7 @@
 #include "cpu/exec/template-start.h"
-
+#include "macro.h"
 #define instr push
-
+/*
 static void do_execute(){
 	uint32_t src = op_src->val;
 	cpu.esp -= 4;
@@ -12,5 +12,31 @@ static void do_execute(){
 make_instr_helper(rm)
 make_instr_helper(r)
 make_instr_helper(i)
+*/
+
+
+static void do_execute() {
+		
+		cpu.esp-=4;
+			MEM_W(cpu.esp,op_src->val);
+				print_asm_template1();
+}
+
+#if DATA_BYTE == 4
+	make_helper(push_i_b)
+{
+			int len = decode_i_b(eip+1);
+						cpu.esp -= DATA_BYTE;
+										MEM_W(cpu.esp, op_src->val);
+															print_asm_template1();
+																					return len+1;
+}
+#endif
+
+make_instr_helper(i)
+#if DATA_BYTE !=1
+	make_instr_helper(rm)
+	make_instr_helper(r)
+#endif
 
 #include "cpu/exec/template-end.h"
