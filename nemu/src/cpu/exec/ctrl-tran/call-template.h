cbbd98990bc36044aee77874cff03b4c926dd7bf
tracer-ics2015 <tracer@njuics.org>
2015-10-31 19:19:18 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:19:18 up  3:08,  3 users,  load average: 0.00, 0.01, 0.05 ff90db1ed2897788436ca1902f7e08d8798579d1

--- nemu/src/cpu/exec/ctrl-tran/call-template.h
+++ nemu/src/cpu/exec/ctrl-tran/call-template.h
@@ -15,5 +15,26 @@ static void do_execute(){
 //#if DATA_BYTE==2||DATA_BYPE==4
 make_instr_helper(i)
 //#endif
-
+/*
+make_helper(call_rm_w)
+{
+	int len=concat(decode_rm_,w)(eip+1);
+	uint32_t src=op_src->val;
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eip+len+1);
+	cpu.eip=src;
+	print_asm_template1();
+	return len+1;
+}
+make_helper(call_rm_l)
+{
+	int len=concat(decode_rm_,l)(eip+1);
+	uint32_t src=op_src->val;
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eip+len+1);
+	cpu.eip=src;
+	print_asm_template1();
+	return len+1;
+}
+*/
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/ctrl-tran/call.c
+++ nemu/src/cpu/exec/ctrl-tran/call.c
@@ -13,3 +13,27 @@
 #undef DATA_BYTE
 
 make_helper_v(call_i)
+
+
+	make_helper(call_rm_w)
+{
+		int len=concat(decode_rm_,w)(eip+1);
+			uint32_t src=op_src->val;
+				cpu.esp-=4;
+					swaddr_write(cpu.esp,4,cpu.eip+len+1);
+						cpu.eip=src;
+							print_asm_template1();
+								return len+1;
+}
+make_helper(call_rm_l)
+{
+		int len=concat(decode_rm_,l)(eip+1);
+			uint32_t src=op_src->val;
+				cpu.esp-=4;
+					swaddr_write(cpu.esp,4,cpu.eip+len+1);
+						cpu.eip=src;
+							print_asm_template1();
+								return len+1;
+}
+
+make_helper_v(call_rm)
