31bce875760a455d0fa03684108e79a34c844805
tracer-ics2015 <tracer@njuics.org>
2015-10-31 16:23:09 +0800

    >  compile NEMU 141242045 song Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:23:09 up 11 min,  3 users,  load average: 0.08, 0.03, 0.02 cd8cdd1b994a178018134f13ecd4d859852f6f3e

--- nemu/src/cpu/exec/string/movs.c
+++ nemu/src/cpu/exec/string/movs.c
@@ -1,8 +1,8 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "movs-template.h"
-#undef DATA_BYTE
+//#define DATA_BYTE 1
+//#include "movs-template.h"
+//#undef DATA_BYTE
 
 //#define DATA_BYTE 2
 //#include "movs-template.h"
@@ -13,3 +13,22 @@
 //#undef DATA_BYTE
 
 //make_helper_v(movs)
+//
+make_helper(movs_b){
+	int databyte=1;
+	swaddr_write(cpu.edi,databyte,swaddr_read(cpu.esi,databyte));
+	if(cpu.DF){cpu.esi-=databyte;cpu.edi-=databyte;}
+	else{cpu.esi+=databyte;cpu.edi+=databyte;}
+	print_asm("movsb");
+	return 1;
+}
+
+make_helper(movs_v){
+	int databyte=4;
+	swaddr_write(cpu.edi,databyte,swaddr_read(cpu.esi,databyte));
+	if(cpu.DF){cpu.esi-=databyte;cpu.edi-=databyte;}
+	else{cpu.esi+=databyte;cpu.edi+=databyte;}
+	print_asm("movsd");
+	return 1;
+}
+
